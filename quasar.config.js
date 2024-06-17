ce4f3056062615dffc433f99872222aa485bf4a9
oKelvinCosta <okelvincosta@gmail.com>
2024-06-17 15:58:09 -0300

    distDir build

--- quasar.config.js
+++ quasar.config.js
@@ -65,7 +65,7 @@ module.exports = configure(function (/* ctx */) {
       // ignorePublicFolder: true,
       // minify: false,
       // polyfillModulePreload: true,
-      // distDir
+
       distDir: 'dist/spa',
 
       // extendViteConf (viteConf) {},
